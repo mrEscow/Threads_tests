a091c4fee9addefdb22047df67979f0c130d88d9
mr.Escow <escowxbox@gmail.com>
2022-09-09 23:53:26 +0300

    std::packaged_task<>

--- main.cpp
+++ main.cpp
@@ -161,6 +161,17 @@ public:
 };
 auto f5 = std::async(move_only());
 //*******************************************************************************
+// Определение частичной специализации std::packaged_task<>
+template<>
+class std::packaged_task<std::string(std::vector<char>*,int)>
+{
+public:
+    template<typename Callable>
+    explicit packaged_task(Callable&& f);
+    std::future<std::string> get_future();
+    void operator()(std::vector<char>*,int);
+};
+//*******************************************************************************
 int main(){
 
     return 0;
