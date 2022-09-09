e04d9bbc77948cce9206362b3580213d894fbd4c
mr.Escow <escowxbox@gmail.com>
2022-09-10 01:30:55 +0300

    Простая реализация функции spawn_task

--- main.cpp
+++ main.cpp
@@ -279,6 +279,20 @@ std::list<T> parallel_quick_sort(std::list<T> input)
     return result;
 }
 //*******************************************************************************
+//  Простая реализация функции spawn_task
+template<typename F,typename A>
+std::future<typename std::result_of<F(A&&)>::type>
+spawn_task(F&& f,A&& a)
+{
+     typedef typename std::result_of<F(A&&)>::type result_type;
+     std::packaged_task<result_type(A&&)>
+     task(std::move(f));
+     std::future<result_type> res(task.get_future());
+     std::thread t(std::move(task),std::move(a));
+     t.detach();
+     return res;
+}
+//*******************************************************************************
 int main(){
 
     return 0;
