2f59bda7228fdd34bbd8c65fe357a6edbaff9f43
mr.Escow <escowxbox@gmail.com>
2022-09-09 15:17:46 +0300

    hierarchical_mutex

--- main.cpp
+++ main.cpp
@@ -123,7 +123,7 @@ public:
 };
 //*******************************************************************************
 // Взаимоблокировка: проблема и решение
-
+//*******************************************************************************
 //  Применение std::lock() и std::lock_guard для реализации операции обмена
 class some_big_object{};
 void swap(some_big_object& lhs,some_big_object& rhs);
@@ -149,8 +149,94 @@ public:
         когда нужно захватить сразу два или более мьютексов , она не в си-
         лах помочь
                     */
+
+//*******************************************************************************
+//  Использование иерархии блокировок для предотвращения взаимоблокировки
+class hierarchical_mutex
+{
+    std::mutex internal_mutex;
+    unsigned long const hierarchy_value;
+    unsigned previous_hierarchy_value;
+    static thread_local unsigned long this_thread_hierarchy_value;
+    void check_for_hierarchy_violation()
+    {
+        if(this_thread_hierarchy_value <= hierarchy_value)
+        {
+            throw std::logic_error("mutex hierarchy violated");
+        }
+    }
+    void update_hierarchy_value()
+    {
+        previous_hierarchy_value = this_thread_hierarchy_value;
+        this_thread_hierarchy_value = hierarchy_value;
+    }
+public:
+    explicit hierarchical_mutex(unsigned long value):
+    hierarchy_value(value),
+    previous_hierarchy_value(0)
+    {}
+    void lock()
+    {
+        check_for_hierarchy_violation();
+        internal_mutex.lock();
+        update_hierarchy_value();
+    }
+    void unlock()
+    {
+        this_thread_hierarchy_value = previous_hierarchy_value;
+        internal_mutex.unlock();
+    }
+    bool try_lock()
+    {
+        // Защита разделяемых данных с помощью мьютексов
+        // Разделение данных между потоками
+        check_for_hierarchy_violation();
+        if(!internal_mutex.try_lock())
+            return false;
+        update_hierarchy_value();
+        return true;
+    }
+};
+thread_local unsigned long
+hierarchical_mutex::this_thread_hierarchy_value(ULONG_MAX);
+//------
+hierarchical_mutex high_level_mutex(10000);
+hierarchical_mutex low_level_mutex(5000);
+
+int do_low_level_stuff();
+
+int low_level_func()
+{
+    std::lock_guard<hierarchical_mutex> lk(low_level_mutex);
+    return do_low_level_stuff();
+}
+void high_level_stuff(int some_param);
+void high_level_func()
+{
+    std::lock_guard<hierarchical_mutex> lk(high_level_mutex);
+    high_level_stuff(low_level_func());
+}
+void thread_a()
+{
+    high_level_func();
+}
+hierarchical_mutex other_mutex(100);
+void do_other_stuff();
+void other_stuff()
+{
+    high_level_func();
+    do_other_stuff();
+}
+void thread_b()
+{
+    // Защита разделяемых данных с помощью мьютексов
+    // Разделение данных между потоками
+    std::lock_guard<hierarchical_mutex> lk(other_mutex);
+    other_stuff();
+}
 //*******************************************************************************
 int main(){
+
     return 0;
 }
 //*******************************************************************************
